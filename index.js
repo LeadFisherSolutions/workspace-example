88bbffae659012198f5ecff8d3b8bbe682b8405a
Alexander Ivanov <sashapop10@yandex.ru>
2023-04-02 17:08:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-import { Logger } from './log/logger.js';
-
-const a = new Logger(() => {});
-a.error('error');
-a.debug('debug');
-a.log('log');
-a.dir({ asd: 123, ad: [12, 32] });
-a.system('system');
-a.access('access');
+console.log("Example work perfect")
