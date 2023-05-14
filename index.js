b4f3b245e4f9fe1b07bc008e4283331c860e8333
sashapop10 <sashapop10@yandex.ru>
2023-05-14 12:42:14 +0300

    Folders

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 'use strict';
 
-module.exports = {};
+const lib = require('/src');
+
+module.exports = { lib };
---
+++ tests/index.test.js
@@ -0,0 +1,6 @@
+const test = require('test');
+const assert = require('assert');
+
+test('First test', () => {
+  assert.strictEqual(1, 1);
+});
